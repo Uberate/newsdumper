6299b02ef7d149c291f7d13fb97c4d7a13519079
Uberate <ubserate@gmail.com>
2023-06-27 10:18:43 +0800

    feat(factory): add factory support

---
+++ pkg/factory/factory.go
@@ -0,0 +1,128 @@
+package factory
+
+import (
+	"github.com/uberate/gset"
+	"sync"
+)
+
+type Entity interface {
+	Kind() string
+	Name() string
+	Version() string
+}
+
+type Generator[T Entity] func(name string, config interface{}) T
+
+type Factory[T Entity] struct {
+	// inner locker
+	locker *sync.RWMutex
+
+	// models 代表工厂中的生成函数，用于初始化目标数据。
+	// kinds-versions-FactoryGenerator
+	models map[string]map[string]Generator[T]
+}
+
+// Registry 注册初 Generator（初始化器），在成功注册后，返回 true，如果当前初始化器已经存在，则不会注册并返回 false。
+func (f *Factory[T]) Registry(kind, version string, g Generator[T]) bool {
+	f.locker.Lock()
+	defer f.locker.Unlock()
+
+	if _, ok := f.models[kind]; !ok {
+		f.models[kind] = map[string]Generator[T]{}
+	}
+
+	if _, ok := f.models[kind][version]; ok {
+		// already exists, return false
+		return false
+	}
+
+	f.models[kind][version] = g
+	return true
+}
+
+func (f *Factory[T]) RemoveVersion(kind, version string) bool {
+	f.locker.Lock()
+	defer f.locker.Unlock()
+
+	if _, ok := f.models[kind]; !ok {
+		return false
+	}
+
+	if _, ok := f.models[kind][version]; !ok {
+		return false
+	}
+	delete(f.models[kind], version)
+	return true
+}
+
+func (f *Factory[T]) RemoveKind(kind string) (int, bool) {
+	f.locker.Lock()
+	defer f.locker.Unlock()
+
+	if _, ok := f.models[kind]; !ok {
+		return 0, false
+	}
+	c := len(f.models[kind])
+
+	delete(f.models, kind)
+	return c, true
+}
+
+func (f *Factory[T]) ContainKind(kind string) bool {
+	f.locker.RLock()
+	defer f.locker.RUnlock()
+	_, ok := f.models[kind]
+	return ok
+}
+
+func (f *Factory[T]) ContainVersion(kind, version string) bool {
+	f.locker.RLock()
+	defer f.locker.RUnlock()
+	_, ok := f.unsafeGetGenerator(kind, version)
+	return ok
+}
+
+func (f *Factory[T]) ListKinds() gset.Set[string] {
+	f.locker.RLock()
+	defer f.locker.RUnlock()
+
+	return gset.FromMapKey(f.models)
+}
+
+func (f *Factory[T]) ListVersions(kind string) gset.Set[string] {
+	f.locker.RLock()
+	defer f.locker.RUnlock()
+
+	if _, ok := f.models[kind]; !ok {
+		return gset.FromArray([]string{})
+	}
+	return gset.FromMapKey(f.models[kind])
+}
+
+func (f *Factory[T]) Get(kind, version, name string, config interface{}) (T, bool) {
+	f.locker.RLock()
+	defer f.locker.RUnlock()
+
+	if g, exists := f.unsafeGetGenerator(kind, version); exists {
+		return g(name, config), true
+	}
+	return nil, false
+}
+
+func (f *Factory[T]) GetGenerator(kind, version string) (Generator[T], bool) {
+	f.locker.RLock()
+	defer f.locker.RUnlock()
+
+	return f.unsafeGetGenerator(kind, version)
+}
+
+func (f *Factory[T]) unsafeGetGenerator(kind, version string) (Generator[T], bool) {
+	if _, ok := f.models[kind]; !ok {
+		return nil, false
+	}
+	if _, ok := f.models[kind][version]; !ok {
+		return nil, false
+	}
+
+	return f.models[kind][version], true
+}
